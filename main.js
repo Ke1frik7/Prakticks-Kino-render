b18c3e6b480fc0a15d0b7ba45c19060f28a73564
Shohijahon-Musinkulovgit config --global user.name Shohijahon-Musinkulov <94446105+Shoha777@users.noreply.github.com>
2022-09-29 14:33:40 +0300

    commits

--- main.js
+++ main.js
@@ -48,6 +48,51 @@ let form = document.getElementById("form")
     form.addEventListener("submit", handle)
     
     let kinoCards = document.querySelector(".kino_cards")
+    let selectSort = document.querySelector(".selects_sort")
+    let objectSort = {
+        az: function(a,b){
+           if(a.title.toLowerCase() < b.title.toLowerCase()){
+                return 1
+           } else{
+            return -1
+           }
+        },
+        za:function(a,b){
+            if(a.title.toLowerCase() < b.title.toLowerCase()){
+                return -1
+            }else{
+                return 1
+            }
+        },
+        rate:function(a,b){
+            if(a.rate < b.rate){
+                return 1
+            }else{
+                return -1
+            }
+        },
+        rates:function(a,b){
+            if(a.rate < b.rate){
+                return -1
+            }else{
+                return 1
+            }
+        },
+        no: function(a,b){
+            if(a.year < b.year){
+                return 1
+            }else{
+                return -1
+            }
+        },
+        on:function(a,b){
+            if(a.rate < b.rate){
+                return -1
+            }else{
+                return 1
+            }
+        },
+    }
     function objectAylan(obj){
         kinoCards.innerHTML = null
         obj.map((e) => {
@@ -64,6 +109,7 @@ let form = document.getElementById("form")
             card.appendChild(cardTitle)
             let h2  = document.createElement("h2")
             h2.appendChild(document.createTextNode(e.title))
+            h2.id = 'title'
             cardTitle.appendChild(h2)
             let h3 = document.createElement("h3")
             h3.appendChild(document.createTextNode(e.year))
@@ -78,6 +124,15 @@ let form = document.getElementById("form")
             a.appendChild(document.createTextNode(`Trailer film ${e.title}`))
             btn.appendChild(a)
             cardTitle.appendChild(btn)
+            let i  = document.createElement("i")
+            i.className = "fa-regular fa-heart"
+            i.style.background  = "transparent"
+            let button = document.createElement("button")
+            button.className = 'leo'
+            button.appendChild(i)
+            button.appendChild(document.createTextNode(e.title))
+            button.addEventListener("click", likes)
+            cardTitle.appendChild(button)
         })
     }
     let results = []
@@ -105,6 +160,7 @@ let form = document.getElementById("form")
         console.log(e)
         e.preventDefault()
         let optionValue = select.value
+        let selectSortValue = selectSort.value
         let filter = []
         if(optionValue == "all"){
             filter = movies
@@ -115,8 +171,56 @@ let form = document.getElementById("form")
         let inputValue = inputSub.value
         let rejex = new RegExp(inputValue, "gi")
         filter = filter.filter((item) => item.title.match(rejex))
+        
+        filter.sort(objectSort[selectSortValue])
+        console.log(filter)
         objectAylan(filter)
     }
     document.getElementById("forms").addEventListener("submit", formSub)
     objectAylan(movies)
+    let objecting = {
+        name : null,
+        img: null,
+        text: null
+    }
+    let local = document.querySelector(".local_div") 
+    let storidge = []
+    let arayem = []
+    let parsen;
+    function likes(e){
+       let text = e.target.textContent
+       let regesS = new RegExp(text, "gi")
+       arayem = movies.filter((item) => item.title.match(regesS))
+       for(let i = 0; i<arayem.length; i++){
+        if(!storidge.includes(arayem[i].bigposter && !storidge.includes(arayem[i].title))){
+            storidge.push(arayem[i].bigposter)
+            storidge.push(arayem[i].title)
+                objecting.name = arayem[i].title
+                objecting.img = arayem[i].bigposter
+                objecting.text = true                     
+            }
+    }
+        localStorage.setItem("storage", JSON.stringify(objecting))
+        console.log(localStorage.getItem("storage"))
+         parsen  = JSON.parse(localStorage.getItem("storage"))
+        console.log(parsen)
+    }
+        let h2 = document.createElement("h2")
+        h2.appendChild(document.createTextNode("Like bosilgan va saqlangan kinolar"))
+        local.appendChild(h2)
+        let dives = document.createElement("div")
+        dives.className = 'localstorage_div'
+        let img = document.createElement("img")
+        img.src = JSON.parse(localStorage.getItem("storage")).img
+        img.className = 'localstorage_img'
+        dives.appendChild(img)
+        let h3 =  document.createElement("h3")
+        h3.appendChild(document.createTextNode(JSON.parse(localStorage.getItem("storage")).name))
+        dives.appendChild(h3)
+        local.appendChild(dives)
     
+        function boss(e){
+        console.log(e)
+        local.classList.toggle("local_div_active")    
+    }
+    document.querySelector(".local_diven").addEventListener("click", boss)
--- object.js
+++ object.js
@@ -6,35 +6,39 @@ let movies = [
         bigposter: "https://images.unsplash.com/photo-1497124401559-3e75ec2ed794?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=870&q=80",
         categories:[
             "Fantastic"
-        ]
+        ],
+        rating: 8
     },
     
     {
-        title: "Boyka",
+        title: "Yuri Boyka",
         year: "2010",
         trailer: "https://youtu.be/A6cgtnHzqoA",
         bigposter: "https://ultimateactionmovies.com/wp-content/uploads/2019/09/Boyka-Undisputed.jpg",
         categories:[
             "Fight Tralogic"
-        ]
+        ],
+        rating: 13
     },
     {
-        title: "Messi",
+        title: "Lionel Messi",
         year: "2016",
         trailer: "https://youtu.be/Ny--3PqxQ1s",
         bigposter: "https://static01.nyt.com/images/2019/04/16/sports/16onsoccerweb-2/merlin_153612873_5bb119b9-8972-4087-b4fd-371cab8c5ba2-superJumbo.jpg?quality=75&auto=webp",
         categories:[
             "Football Mativation"
-        ]
+        ],
+        rating: 16
     },
     {
-        title: "Ronaldo",
+        title: "Cristiano Ronaldo",
         year: "2018",
         trailer: "https://youtu.be/qc8C-rH2nqA",
         bigposter: "https://phantom-marca.unidadeditorial.es/c56848cfee85df94876073904cfdd799/resize/660/f/webp/assets/multimedia/imagenes/2022/07/30/16591778851898.jpg",
         categories:[
             "Football Mativation"
-        ]
+        ],
+        rating: 7
     },
     {
         title: "Bill Gates",
@@ -43,7 +47,8 @@ let movies = [
         bigposter: "https://assets.entrepreneur.com/content/3x2/2000/20171204143532-GettyImages-850154658.jpeg?auto=webp&quality=95&crop=16:9&width=675",
         categories:[
             "IT motivation Microsoft"
-        ]
+        ],
+        rating: 11
     },
     {
         title: "Stive jobs",
@@ -52,16 +57,18 @@ let movies = [
         bigposter: "https://www.albawaba.com/sites/default/files/styles/d08_standard/public/im/getty.images/google.steve.jobs.jpg?itok=-kCnzs6e",
         categories:[
             "IT motivation Apple"
-        ]
+        ],
+        rating: 12
     },
     {
-        title: "Undertaker",
+        title: "The Undertaker",
         year: "2022",
         trailer: "https://youtu.be/ldgpvlN8XL4",
         bigposter: "https://www.planetawrestling.com/wp-content/uploads/The-Undertaker-programa-1000.jpg",
         categories:[
             "WWE Super"
-        ]
+        ],
+        rating: 14
     },
     {
         title: "Mark Suckerberg FaceBook",
@@ -70,7 +77,8 @@ let movies = [
         bigposter: "https://p7z2w8n8.rocketcdn.me/wp-content/uploads/2020/11/mark-zuckerberg-quem-e-carreira-vida-pessoal-e-criacao-do-facebook-3-1024x768.jpg.webp",
         categories:[
             "IT motivation FaceBook"
-        ]
+        ],
+        rating: 10
     },
     {
         title: "Jumanji",
@@ -79,7 +87,8 @@ let movies = [
         bigposter: "https://wmimages.bruzz.be/styles/3a4d8eb42ce3578819a2af49730fcefc650485a9/bulk_image_upload/1517297505/BRUZZ-out%201601/1601%20FILM%20jumanji.jpg?style=W3sicmVzaXplIjp7ImZpdCI6ImNvdmVyIiwid2lkdGgiOjY0MCwiaGVpZ2h0IjpudWxsLCJncmF2aXR5IjpudWxsLCJ3aXRob3V0RW5sYXJnZW1lbnQiOmZhbHNlfX0seyJqcGVnIjp7InF1YWxpdHkiOjk1fX1d&sign=37d18235e183cee3a4eb0335e3e10793e409b33699a3650e6bd3580914fabe24",
         categories:[
             "Fantastic"
-        ]
+        ],
+        rating: 9
     },
     {
         title: "Bleyd",
@@ -88,7 +97,8 @@ let movies = [
         bigposter: "https://2.bp.blogspot.com/-ytmkhJZ6kL4/XHzcwlEhYiI/AAAAAAAAuRg/kd3pIYmom_0Cc37YDIBLBPya_G1h_5eSwCLcBGAs/s1600/Blade.jpg",
         categories:[
             "Vampir"
-        ]
+        ],
+        rating: 7
     },
     {
         title: "Bruss Lee",
@@ -97,7 +107,8 @@ let movies = [
         bigposter: "https://dynamic-media-cdn.tripadvisor.com/media/photo-o/0d/cb/e7/2d/bruce-lee-statue.jpg?w=1200&h=-1&s=1",
         categories:[
             "Legend"
-        ]
+        ],
+        reting: 8
     },
     {
         title: "HACKER",
@@ -106,6 +117,7 @@ let movies = [
         bigposter: "https://www.preemptive.com/wp-content/uploads/2020/10/hacker-1200x639.png",
         categories:[
             "Hack kino"
-        ]
+        ],
+        rating: 10
     },
 ]
